b9ad8e3f407c4b5ba243dcac5cceedaa98a4afa6
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-03-06 16:00:29 -0300

    fix: testa vite

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,12 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-
-function nonBlockingCssPlugin() {
-  return {
-    name: 'non-blocking-css',
-    enforce: 'post',
-    transformIndexHtml(html) {
-      return html.replace(
-        /<link rel="stylesheet"([^>]+)>/g,
-        `<link rel="stylesheet"$1 media="print" onload="this.media='all'">`
-      )
-    },
-  }
-}
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), nonBlockingCssPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
